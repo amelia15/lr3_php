f4c742edf5b1a2a6e4c9d9aae18fcbeb044d994b
amelia15 <rina.sl@ukr.net>
2019-11-09 15:37:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
 
+class Model2
+{
+    public $string1;
+ 
+    public function __construct(){
+              $this->string1 = "<strong>Главная область применения PHP</strong> - написание скриптов, работающих на стороне сервера; таким образом, PHP способен выполнять все то, что выполняет любая другая программа CGI, например, обрабатывать данные форм, генерировать динамические страницы или отсылать и принимать cookies.";
+    }
+}
+
 class Model
 {
-    public $string;
-    
+    public $string1;
 
     public function __construct(){
-        $this->string = "<strong>PHP </strong>— рефлексивный язык программирования, разработанный в качестве инструмента для создания динамических веб-страниц и работы с базами данных.";
-    }
+        $this->string1 = "<strong>PHP </strong>—  это широко используемый язык сценариев общего назначения с открытым исходным кодом, разработанный в качестве инструмента для создания динамических веб-страниц и работы с базами данных.";
+        
 }
 
 class View
@@ -20,8 +28,8 @@ class View
         $this->model = $model;
     }
 	
-    public function output(){
-        return "<p>" . $this->model->string . "</p>";
+    public function output1(){
+        return "<p>" . $this->model->string1 . "</p>";
     }
 }
 
@@ -39,9 +47,24 @@ $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
 
-if (isset($_GET['action']) && !empty($_GET['action'])) {
-    $controller->{$_GET['action']}();
+$model2 = new Model2();
+$controller2 = new Controller($model2);
+$view2 = new View($controller2, $model2);
+if (isset($_POST["definition"]))
+{
+    if (isset($_GET['action']) && !empty($_GET['action'])) {
+        $controller->{$_GET['action']}();
+    }
+    
+    echo $view->output1();
 }
 
-echo $view->output();
+if (isset($_POST["application_areas"]))
+{
+    if (isset($_GET['action']) && !empty($_GET['action'])) {
+        $controller2->{$_GET['action']}();
+    }
+    
+    echo $view2->output1();
+}
 ?>
